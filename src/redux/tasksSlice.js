9021103fbcab632d52c4305d0f7d862eed53c0a1
IrynaVal <piv8050@gmail.com>
2023-04-05 22:32:36 +0300

    part1-add createAsyncThunk

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,39 +1,71 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { fetchTasks } from './operations';
 
-const tasksInitialState = [];
+const tasksInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
 
 const tasksSlice = createSlice({
   name: 'tasks',
   initialState: tasksInitialState,
-  reducers: {
-    addTask: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(text) {
-        return {
-          payload: {
-            text,
-            id: nanoid(),
-            completed: false,
-          },
-        };
-      },
+  extraReducers: {
+    [fetchTasks.pending](state) {
+      state.isLoading = true;
     },
-    deleteTask(state, action) {
-      const index = state.findIndex(task => task.id === action.payload);
-      state.splice(index, 1);
+    [fetchTasks.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
     },
-    toggleCompleted(state, action) {
-      for (const task of state) {
-        if (task.id === action.payload) {
-          task.completed = !task.completed;
-          break;
-        }
-      }
+    [fetchTasks.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
     },
   },
+  // reducers: {
+  //   fetchingInProgress(state) {
+  //     state.isLoading = true;
+  //   },
+  //   fetchingSuccess(state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items = action.payload;
+  //   },
+  //   fetchingError(state, action) {
+  //     state.isLoading = false;
+  //     state.error = action.payload;
+  //   },
+
+  // addTask: {
+  //   reducer(state, action) {
+  //     state.push(action.payload);
+  //   },
+  //   prepare(text) {
+  //     return {
+  //       payload: {
+  //         text,
+  //         id: nanoid(),
+  //         completed: false,
+  //       },
+  //     };
+  //   },
+  // },
+  // deleteTask(state, action) {
+  //   const index = state.findIndex(task => task.id === action.payload);
+  //   state.splice(index, 1);
+  // },
+  // toggleCompleted(state, action) {
+  //   for (const task of state) {
+  //     if (task.id === action.payload) {
+  //       task.completed = !task.completed;
+  //       break;
+  //     }
+  //   }
+  // },
 });
 
-export const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
+export const { fetchingInProgress, fetchingSuccess, fetchingError } =
+  tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
